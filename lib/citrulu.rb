b78d8367040058d681652df50a5cf0d13d4a0583
Duncan Stuart <duncan@dxw.com>
2012-10-23 20:31:23 +0100

    Cleaning up base file

--- lib/citrulu.rb
+++ lib/citrulu.rb
@@ -1,15 +1,11 @@
-BASE_URL = "https://www.citrulu.com/api"
-API_VERSION = "v1"
-
-# Helper methods to write:
-# .prepend, .append - TDD THEM!!!
-
 require File.expand_path('../citrulu/test_file', __FILE__)
 require 'faraday'
 
-module API
+module Citrulu
+  BASE_URL = "https://www.citrulu.com/api/v1"
+  
   def self.connection
-    Faraday.new(:url => "#{BASE_URL}/#{API_VERSION}") do |faraday|
+    Faraday.new(:url => BASE_URL) do |faraday|
       faraday.request  :url_encoded             # form-encode POST params
       faraday.response :logger                  # log requests to STDOUT
       faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
--- spec/citrulu/test_file_spec.rb
+++ spec/citrulu/test_file_spec.rb
@@ -11,7 +11,7 @@ describe "TestFile" do
     
     # Faraday.stub(:new).and_return(@connection)
     
-    API.stub(:connection).and_return(@connection)    
+    Citrulu.stub(:connection).and_return(@connection)    
     stub_const("CITRULU_API_KEY", "Foo")
     
     @response = double("Response")
