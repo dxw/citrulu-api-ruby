dbe736573c9760cab7fdda5f1d8bea384dab92d5
Duncan Stuart <duncan@dxw.com>
2012-10-24 12:55:29 +0100

    cleaning up base file

--- lib/citrulu.rb
+++ lib/citrulu.rb
@@ -3,13 +3,15 @@ require 'faraday'
 
 module Citrulu
   BASE_URL = "https://www.citrulu.com/api/v1"
+  # BASE_URL = "http://localhost:3000/api/v1"
   
   def self.connection
-    Faraday.new(:url => BASE_URL) do |faraday|
-      faraday.request  :url_encoded             # form-encode POST params
-      faraday.params =      { 'auth_token' => CITRULU_API_KEY }
-      faraday.response :logger                  # log requests to STDOUT
-      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+    Faraday.new(:url => BASE_URL) do |connection|
+      connection.request   :url_encoded             # form-encode POST params
+      connection.response  :logger                  # log requests to STDOUT
+      connection.adapter   Faraday.default_adapter  # make requests with Net::HTTP
+      
+      connection.params[:auth_token] = CITRULU_API_KEY # Authenticate using the user's API key
     end
   end
 end
